8b8c38f65849f5bc157029635c28453d7b8988b0
unknown <janecha>
2023-10-16 16:52:43 +0800

    commit pertama saya

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+	// define path constants
+    define("APP_PATH", "http://localhost/unkpresent/public");
+
+    // define database config
+    define("HOST_DB", "localhost");
+    define("NAME_DB", "phpmyadmin");
+    define("USER_DB", "root");
+    define("PASS_DB", "");
+	
+	
+?>
